2f318563fc0d1b4d4bec99eedbf275cc9a1e3831
Atul Dhanotiya <atuldhanotiya7@gmail.com>
2026-05-27 13:39:54 +0530

    Refactor router configuration in routes.ts
    Refactored router configuration to improve readability.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,20 +6,21 @@ import { ProductDetailPage } from "./pages/ProductDetailPage";
 import { AboutPage } from "./pages/AboutPage";
 import { ContactPage } from "./pages/ContactPage";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Root,
-    children: [
-      { index: true, Component: HomePage },
-      { path: "products", Component: ProductsPage },
-      { path: "products/:productId", Component: ProductDetailPage },
-      { path: "categories", Component: ProductsPage },
-      { path: "about", Component: AboutPage },
-      { path: "contact", Component: ContactPage },
-    ],
-  },
-], 
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Root,
+      children: [
+        { index: true, Component: HomePage },
+        { path: "products", Component: ProductsPage },
+        { path: "products/:productId", Component: ProductDetailPage },
+        { path: "categories", Component: ProductsPage },
+        { path: "about", Component: AboutPage },
+        { path: "contact", Component: ContactPage },
+      ],
+    },
+  ],
   {
     basename: "/SSA",
   }
