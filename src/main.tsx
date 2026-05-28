2bbedc31326bb1f7dec7c8f351d8573c77fa3f6a
Atul Dhanotiya <atuldhanotiya7@gmail.com>
2026-05-28 09:26:32 +0530

    Implement redirect logic based on sessionStorage

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,11 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, "", redirect);
+}
   createRoot(document.getElementById("root")!).render(<App />);
   
